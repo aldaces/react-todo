cd3723e7edcf6145a694a844604618a7ddb576e7
kolesnikov <shadarsi@gmail.com>
2019-07-11 13:05:19 +0300

    todo-list-item

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 
-const TodoListItem = () => {
+const TodoListItem = ( {label, important = false} ) => {
+
+    const style = {
+        color: important ? 'tomato' : 'black'
+    };
+
     return (
-        <span>Drink Cofee</span>
+        <span style={ style }>{ label }</span>
     )
 }
 
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import TodoListItem from './todo-list-item'
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+
+    const element = todos.map((item) => {
+        return (
+            <li>
+                <TodoListItem label={item.label} important={item.important}/>
+            </li>
+        )
+    })
+
     return (
         <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+            { element }
         </ul>
     )
 }
